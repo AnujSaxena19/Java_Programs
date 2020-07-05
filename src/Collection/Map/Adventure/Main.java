5d77837f6ab26d5f005962c75d18514f31bf725c
AnujSaxena19 <19anujsaxena19@gmail.com>
2020-07-05 23:24:22 +0530

    Adventure Game.

--- src/Collection/Map/Adventure/Main.java
+++ src/Collection/Map/Adventure/Main.java
@@ -15,7 +15,24 @@ public class Main {
         locations.put(1, new Location(1,"You are standing at the end of the road"));
         locations.put(2, new Location(2,"You are at the top of the hill"));
         locations.put(3, new Location(3,"You are inside a buliding"));
-        locations.put(4, new Location(4,"You are in a forest"));
+        locations.put(4, new Location(4,"You are in a valley"));
+        locations.put(5, new Location(5,"You are in a forest"));
+
+        locations.get(1).addExit("W",2);
+        locations.get(1).addExit("E",3);
+        locations.get(1).addExit("S",4);
+        locations.get(1).addExit("N",5);
+
+        locations.get(2).addExit("N",5);
+
+        locations.get(3).addExit("W",1);
+
+        locations.get(4).addExit("N",1);
+        locations.get(4).addExit("W",2);
+
+        locations.get(5).addExit("S",1);
+        locations.get(5).addExit("W",2);
+
 
         int loc=1;
         while (true){
@@ -24,8 +41,18 @@ public class Main {
                 break;
             }
 
-            loc=sc.nextInt();
-            if(!locations.containsKey(loc)){
+            Map<String,Integer> exits=locations.get(loc).getExits();
+            System.out.println("Available exits are ");
+            for(String exit: exits.keySet()){
+                System.out.print(exit+ ",");
+            }
+            System.out.println();
+
+            String direction=sc.nextLine().toUpperCase();
+            if(!locations.containsKey(direction)) {
+                loc = exits.get(direction);
+            }
+            else{
                 System.out.println("You cannot go in direction");
             }
         }
